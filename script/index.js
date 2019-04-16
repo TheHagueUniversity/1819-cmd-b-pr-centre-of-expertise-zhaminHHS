c459b38ddf979891de9cdb2bda0090a5df72146d
Zhamin Magdalena <14048159@student.hhs.nl>
2019-04-16 19:01:12 +0200

    latestt version

--- script/index.js
+++
@@ -1,5 +0,0 @@
-function subsidieButtonClick() {
-  var img = document.createElement('img');
- img.setAttribute('src subsidie aanvragen.html');
-  document.getElementById("Main info").appendChild(img);
-}
